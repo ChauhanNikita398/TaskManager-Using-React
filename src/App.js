b84ef06c77141694be1103860f72b5a08b7fdbb8
ChauhanNikita398 <48960880+ChauhanNikita398@users.noreply.github.com>
2022-01-31 23:55:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 import TodoList from './components/TodoList';
 import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div className="App">
-        {/* <div className="header text-center" >
-          <h3 >Todo List</h3>
-          <button className= "btn btn-primary mt-2" >Create New category</button>
-        </div> */}
+        
       
       <TodoList/>
     </div>
